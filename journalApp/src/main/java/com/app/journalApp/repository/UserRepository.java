a9179a18f2f19cea2187dc0fa0101d3171813d46
ArunDot18 <arunkargatia47@gmail.com>
2025-01-16 19:09:17 +0530

    base

---
+++ journalApp/src/main/java/com/app/journalApp/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.app.journalApp.repository;
+
+import com.app.journalApp.entity.JournalEntry;
+import com.app.journalApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    User findByUserName(String username);
+
+    void deleteByUserName(String name);
+}
+
